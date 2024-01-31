31053ed01924c42a24b821cd8909431b98083384
WINDOWS-Q8ENQ74\WD <dream9505@gmail.com>
2024-01-31 18:51:05 +0900

    클라임클라우드캣 1.백그라운드 바깥쪽막기 2.메인카메라가 플레이어를 따라가게하기 3.점프가 공중에서도 여러번 반복하는걸 막기 작업중

--- Assets/Scripts/ClimbScripts/CatController.cs
+++ Assets/Scripts/ClimbScripts/CatController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CatController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rbody;
-    [SerializeField] private float moveForce = 1f;
+    [SerializeField] private float moveForce = 3f;
     [SerializeField] private float jumpForce = 680f;
 
     [SerializeField] private ClimbCloudGameDirector gameDirector;
 
+    private Animator anim;
+
     private void Start()
     {
-        
+        //this.gameObject.GetComponent<Animator>();
+        this.anim = GetComponent<Animator>();
+        anim.speed = moveForce;
     }
 
     void Update()
@@ -35,26 +40,36 @@ public class CatController : MonoBehaviour
             dirX = 1;
         }
         //방향
-        Debug.Log(dirX);  //방향 -1, 0, 1
+        //Debug.Log(dirX);  //방향 -1, 0, 1
 
         //스케일 X를 변경 하는데 키가 눌렸을 때만
         //키가 눌렸을때만 = (dirX != 0)
         if(dirX != 0)
         {
             this.transform.localScale = new Vector3(dirX, 1, 1);
-        }       
-        
-        
-
+        }
         // 벡터의 곱   //벡터3
-        Debug.Log(this.transform.right * dirX);
+        // Debug.Log(this.transform.right * dirX);
 
         //속도 제한
         if ( Mathf.Abs(this.rbody.velocity.x) < 2.0)
         {
             this.rbody.AddForce(this.transform.right * dirX * moveForce);
         }
-        
+
+        this.anim.speed = (Mathf.Abs(this.rbody.velocity.x) / 2f);
         this.gameDirector.UpdateVelocityText(this.rbody.velocity);
+    }
+
+    //Triger 모드일경우 충돌 판정을 해주는 이벤트 함수
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //최초로 충돌할때 한번만 호출
+        //OntriggerExit2d = 나올때 한번만 호출
+        //stay2d = 충돌중일때 계속 호출
+        Debug.LogFormat("OnTriggerEnter2D: {0}", collision);
+
+        //장면을 전환
+        SceneManager.LoadScene("ClimbCloudClear");
     }
 }
